a1ae83e7f3a04f540bc360c020c70453b6cb3ebf
Ali Gümüş Tosun <ali.t@mindojo.com>
2021-03-03 17:12:48 +0300

    PD2-2781:added eslintrc,new js-styleguide and eslint https://mindojo.atlassian.net/browse/PD2-2781

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended',
+    './node_modules/mindojo-js-styleguide/configs/mindojo',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {},
+};
